3d175913c13b436203de0cd00861a1ee73297847
Antonio Geraldo Cardoso Junior <>
2022-01-19 00:02:38 -0300

    Criando Entidade Pessoa

--- src/main/java/com/example/algamoney/api/model/Pessoa.java
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -1,26 +1,32 @@
 package com.example.algamoney.api.model;
 
+import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "categoria")
-public class Categoria{
+public class Pessoa implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
 	@NotNull
-	@Size(min=3, max=30)
 	private String nome;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	@NotNull
+	private Boolean ativo;
 
 	public Long getCodigo() {
 		return codigo;
@@ -38,9 +44,29 @@ public class Categoria{
 		this.nome = nome;
 	}
 
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(codigo, nome);
+		return Objects.hash(codigo);
 	}
 
 	@Override
@@ -51,8 +77,8 @@ public class Categoria{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
-		return Objects.equals(codigo, other.codigo) && Objects.equals(nome, other.nome);
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(codigo, other.codigo);
 	}
 	
 }
